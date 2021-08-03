7a545cb243796d4d91ff2595a9cb4d117a876461
preetithirukonda <preetithirukonda@gmail.com>
2021-08-03 14:11:34 -0400

    fixed bug

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ new_file = input("Enter the name of the file to be compared: ")
 # base_file = 'ASR9K-SR-ISIS-v662.xml'
 # new_file = 'ASR9K-SR-ISIS-v731.xml'
 
-base_file = 'SNMPIFINDEX-Baseline.xml'
-new_file = 'SNMPIFINDEX-Compare.xml'
+# base_file = 'SNMPIFINDEX-Baseline.xml'
+# new_file = 'SNMPIFINDEX-Compare.xml'
 
 # 2) parsing the file
 base = ET.parse(base_file)
